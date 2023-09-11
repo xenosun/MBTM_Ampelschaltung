db604b6bf5d8159173356037c17ebcad74eacc8b
Dr.-Ing. Martin Richter <martin.richter@web.de>
2023-09-11 17:49:34 +0200

    checked version

--- sketch_ampelschaltung/sketch_ampelschaltung.ino
+++ sketch_ampelschaltung/sketch_ampelschaltung.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+int ampelRotPin = 12;
+int ampelGelbPin = 11;
+int ampelGruenPin = 10;
 
+int fgRotPin = 9;
+int fgGruenPin = 8;
+
+void setup() {
+  pinMode(ampelRotPin, OUTPUT);
+  pinMode(ampelGelbPin, OUTPUT);
+  pinMode(ampelGruenPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // gruen
+  digitalWrite(ampelRotPin, LOW);
+  digitalWrite(ampelGelbPin, LOW);
+  digitalWrite(ampelGruenPin, HIGH);
+  digitalWrite(fgRotPin, HIGH);
+  digitalWrite(fgGruenPin, LOW);
+  delay(5000);
+  // gelb
+  digitalWrite(ampelRotPin, LOW);
+  digitalWrite(ampelGelbPin, HIGH);
+  digitalWrite(ampelGruenPin, LOW);
+  digitalWrite(fgRotPin, HIGH);
+  digitalWrite(fgGruenPin, LOW);
+  delay(1000);
+  // rot
+  digitalWrite(ampelRotPin, HIGH);
+  digitalWrite(ampelGelbPin, LOW);
+  digitalWrite(ampelGruenPin, LOW);
+  digitalWrite(fgRotPin, LOW);
+  digitalWrite(fgGruenPin, HIGH);
+  delay(5000);
+  // rot-gelb
+  digitalWrite(ampelRotPin, HIGH);
+  digitalWrite(ampelGelbPin, HIGH);
+  digitalWrite(ampelGruenPin, LOW);
+  digitalWrite(fgRotPin, HIGH);
+  digitalWrite(fgGruenPin, LOW);
+  delay(1000);
 }
